9e8833367a1a21b146b98e86bc13ee349c5f0630
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-01-07 15:45:06 +0900

    fix: schedule 페이지에서 token을 받을 수 있게 변경

--- just-swim-app/src/app/(afterLogin)/schedule/page.tsx
+++ just-swim-app/src/app/(afterLogin)/schedule/page.tsx
@@ -1,5 +1,46 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useUserStore } from '@store';
+import { getTokenInCookies, setTokenInCookies } from '@utils';
+import { redirect, useSearchParams, useRouter } from 'next/navigation';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 export default function Schedule() {
-  redirect('/schedule/weekly');
+  const params = useSearchParams().get('token');
+  const [token, setToken] = useState<string>();
+  const router = useRouter();
+
+  const { setAddUserToken } = useUserStore();
+
+  useEffect(() => {
+    const checkCookies = async () => {
+      const isLogin = await getTokenInCookies();
+
+      if (!isLogin) {
+        return;
+      } else {
+        router.push('/schedule/weekly');
+      }
+    };
+
+    checkCookies();
+  }, [router]);
+
+  useLayoutEffect(() => {
+    const checkToken = async () => {
+      if (params) {
+        const newToken = await setTokenInCookies(params);
+        setAddUserToken(newToken);
+        setToken(newToken);
+      }
+    };
+    checkToken();
+  }, [params, setAddUserToken]);
+  useLayoutEffect(() => {
+    if (token) {
+      redirect('/schedule/weekly');
+    }
+  }, [token]);
+
+  return null;
 }
