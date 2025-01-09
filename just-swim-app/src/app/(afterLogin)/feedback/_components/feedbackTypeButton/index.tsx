66b6eac9dde448c0a22b72d3ffc1c67b9482411e
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-01-09 15:52:35 +0900

    fix: feedbackTypeButton scss 수정

--- just-swim-app/src/app/(afterLogin)/feedback/_components/feedbackTypeButton/index.tsx
+++ just-swim-app/src/app/(afterLogin)/feedback/_components/feedbackTypeButton/index.tsx
@@ -6,19 +6,16 @@ import { useRouter } from 'next/navigation';
 import { useUserStore } from '@store';
 
 import styled from './feedbackTypeButton.module.scss';
-import { getMyProfile } from '@apis';
 
 export function FeedbackTypeButton({ token }: { token: string }) {
   const router = useRouter();
+  const { getUserType } = useUserStore();
   const [userType, setUserType] = useState<string | null>(null);
 
   useEffect(() => {
-    const getUserType = async () => {
-      const type = (await getMyProfile()).data.data.userType;
-      setUserType(type);
-    };
-    getUserType();
-  }, [token]);
+    const type = getUserType(token);
+    setUserType(type);
+  }, [token, getUserType]);
 
   const handleIndividualClick = (feedbackType: string) => {
     router.push(`feedback/create/${feedbackType}`);
@@ -31,7 +28,9 @@ export function FeedbackTypeButton({ token }: { token: string }) {
         userType === 'instructor' && (
           <div className={styled.button_box}>
             {/* personal */}
-            <button onClick={() => handleIndividualClick('person')}>
+            <button
+              onClick={() => handleIndividualClick('person')}
+              className={styled.button_person}>
               <p className={styled.button_title}>
                 <span>개별</span>
                 <br />
@@ -40,7 +39,9 @@ export function FeedbackTypeButton({ token }: { token: string }) {
               <p className={styled.button_desc}>개인 작성에 유용합니다.</p>
             </button>
             {/* grouop */}
-            <button onClick={() => handleIndividualClick('class')}>
+            <button
+              onClick={() => handleIndividualClick('class')}
+              className={styled.button_class}>
               <p className={styled.button_title}>
                 <span>반별</span>
                 <br />
