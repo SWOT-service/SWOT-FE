8582519bd23180cb4ceb8c8875062b8c81e19865
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-01-30 19:38:59 +0900

    fix: feedbackTargetListModal scss 수정

--- just-swim-app/src/app/(afterLogin)/feedback/_components/feedbackTargetListModal/index.tsx
+++ just-swim-app/src/app/(afterLogin)/feedback/_components/feedbackTargetListModal/index.tsx
@@ -3,6 +3,7 @@
 import { FeedbackTargetListModalProps } from '@types';
 import styled from './feedbackTargetListModal.module.scss';
 import { ModalBody } from '@components';
+import Image from 'next/image';
 
 export function FeedbackTargetListModal({
   feedbackTargetList,
@@ -11,23 +12,31 @@ export function FeedbackTargetListModal({
   return (
     <ModalBody hideModal={hideModal}>
       <div className={styled.modal}>
-        <div className={styled.modal_title}>
-          <div className={styled.member_list_title}>
-            <p>피드백 대상</p>
-          </div>
-          <div className={styled.member_list_wrapper}>
-            {feedbackTargetList.map((target, index) => (
-              <li key={index} className={styled.member_list_item}>
-                <div
-                  className={styled.profileImg}
-                  style={{
-                    backgroundImage: `url(${target.memberProfileImage})`,
-                  }}></div>
-                <div>{target.memberName}</div>
-                <div>{target.memberProfileImage}</div>
-              </li>
-            ))}
-          </div>
+        <div className={styled.member_list_title}>
+          <p>피드백 대상</p>
+        </div>
+        <div className={styled.member_list_wrapper}>
+          {feedbackTargetList.map((target, index) => (
+            <div key={index} className={styled.member_list_item}>
+              <div className={styled.image_wrapper}>
+                {target.memberProfileImage &&
+                target.memberProfileImage.startsWith('http') ? (
+                  <Image
+                    src={target.memberProfileImage}
+                    alt={target.memberName}
+                    width={34}
+                    height={34}
+                  />
+                ) : (
+                  <div className={styled.empty_image} />
+                )}
+              </div>
+              <p className={styled.name}>{target.memberName}</p>
+              <div className={styled.lecture}>
+                <p>{target.lectureTitle}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </ModalBody>
