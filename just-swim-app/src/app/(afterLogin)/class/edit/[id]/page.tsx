dd66f545cd3329a1600d4beb65e3c6fa84b2b007
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-02-17 16:41:47 +0900

    fix: ClassInfoEdit response 처리 수정

--- just-swim-app/src/app/(afterLogin)/class/edit/[id]/page.tsx
+++ just-swim-app/src/app/(afterLogin)/class/edit/[id]/page.tsx
@@ -74,8 +74,10 @@ export default function ClassInfoEdit() {
       });
       const updatedLectureData = await response.json();
       setLecture(updatedLectureData);
-      alert('수정되었습니다.');
-      router.push(`/class/detail/${lectureId}`);
+      if (response.ok) {
+        alert('수정되었습니다.');
+        router.push(`/class/detail/${lectureId}`);
+      }
     } catch (error) {
       alert('수정 중 오류가 발생했습니다.');
     }
